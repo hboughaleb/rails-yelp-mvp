4ee822d613bf8b86e8c1e8589b3f10aa77a18482
Hamza Boughaleb <hamza.boughaleb@gmail.com>
2017-11-01 12:24:36 -0400

    Created controller views and routes for controller. Seeded the db

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,5 @@
 Rails.application.routes.draw do
-  get 'restaurants/index'
+  resources :restaurants
 
-  get 'restaurants/show'
-
-  get 'restaurants/new'
-
-  get 'restaurants/create'
-
-  get 'restaurants/edit'
-
-  get 'restaurants/update'
-
-  get 'restaurants/destroy'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'restaurants#index'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    phone_number: "011-124-5356",
+    category:     "french"
+  },
+  {
+    name:         "La truffière",
+    address:      "4 rue Blainville 75005 Paris",
+    phone_number: "012-527-6332",
+    category:     "french"
+  },
+  {
+    name:         "Wok Cafe",
+    address:      "route de Suresnes 75016 Paris",
+    phone_number: "014-530-0913",
+    category:     "japanese"
+  },
+  {
+    name:         "De Emma",
+    address:      "4564 St-Laurent Montreal",
+    phone_number: "514-992-1252",
+    category:     "italian"
+  },
+  {
+    name:         "Mon Nan",
+    address:      "432 Parc Montreal",
+    phone_number: "514-292-2929",
+    category:     "chinese"
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
